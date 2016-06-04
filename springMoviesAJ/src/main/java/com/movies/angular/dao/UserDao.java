9024109a6528b081933a4f58fac1ed75e1904d26
dheeraj1022 <dheeraj.dudipala@gmail.com>
2016-06-04 18:16:26 -0500

    added userDao and userDaoImpl classes

---
+++ springMoviesAJ/src/main/java/com/movies/angular/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.movies.angular.dao;
+
+import java.util.List;
+
+import com.movies.angular.Model.User;
+
+public interface UserDao {
+	
+	public void insertUser();
+	public User getUser(String username);
+	public List<User> getAllUsers();
+	public void deleteUser(String username);
+
+}
---
+++ springMoviesAJ/src/main/java/com/movies/angular/dao/UserDaoImpl.java
@@ -0,0 +1,29 @@
+package com.movies.angular.dao;
+
+import java.util.List;
+
+import com.movies.angular.Model.User;
+
+public class UserDaoImpl implements UserDao {
+
+	public void insertUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteUser(String username) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
