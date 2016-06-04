514803ee4ec37289205dcc421ab5d506ec81eaf4
dheeraj1022 <dheeraj.dudipala@gmail.com>
2016-06-04 18:02:48 -0500

    Added user Model

--- springMoviesAJ/src/main/java/com/movies/angular/Controllers/MainController.java
+++ springMoviesAJ/src/main/java/com/movies/angular/Controllers/MainController.java
@@ -19,7 +19,7 @@ public class MainController {
     public String getRegister() {
 		
 		
-        return "index";
+        return "home";
     }
 	
 	@RequestMapping(value="/insert")
---
+++ springMoviesAJ/src/main/java/com/movies/angular/Model/User.java
@@ -0,0 +1,53 @@
+package com.movies.angular.Model;
+
+public class User {
+	
+	String userName;
+	String password;
+	String firstName;
+	String lastName;
+	String email;
+	String phone;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+
+}
